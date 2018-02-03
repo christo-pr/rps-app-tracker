1240140ae9a6c30f4075caebf11f12e934576946
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-03 09:49:06 -0600

    app/app.module.ts: Removed the default components from the declarations and providers arrays

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,16 +3,6 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
 
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-
-// Uncomment and add to NgModule imports if you need to use two-way binding
-// import { NativeScriptFormsModule } from "nativescript-angular/forms";
-
-// Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
-// import { NativeScriptHttpModule } from "nativescript-angular/http";
-
 @NgModule({
     bootstrap: [
         AppComponent
@@ -23,17 +13,11 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
     ],
-    providers: [
-        ItemService
-    ],
+    providers: [ ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
 })
-/*
-Pass your application module to the bootstrapModule function located in main.ts to start your app
-*/
+
 export class AppModule { }

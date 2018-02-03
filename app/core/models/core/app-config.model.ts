8cc0711c04231adf886914952c53812c74998d3d
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-03 16:01:00 -0600

    app/core/models/: Moved some modules from shared folder to core folder

---
+++ app/core/models/core/app-config.model.ts
@@ -0,0 +1,12 @@
+import { TypeProvider } from '@angular/core';
+import { LoggingLevelEnum } from './../enum/logging-level.enum';
+
+export type AppType = 'Ns' | 'Web';
+
+export interface AppConfig {
+  AppType: AppType;
+  apiEndpoint: string;
+  loggingEnabled: boolean;
+  loggingLevel: LoggingLevelEnum;
+  storageServiceClass: TypeProvider
+}

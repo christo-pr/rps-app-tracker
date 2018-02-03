7c388547de902db5b5bbf5f576e0992cab83240c
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-03 14:31:53 -0600

    app/core/core.module.ts: Created the CoreModule

---
+++ app/core/core.module.ts
@@ -0,0 +1,25 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [],
+  providers: [],
+})
+export class CoreModule {
+
+  /**
+   * Creates an instance of CoreModule and prevent any other instance
+   * @param {CoreModule} parentModule
+   * @memberof CoreModule
+   */
+  constructor (
+    @Optional() @SkipSelf() parentModule: CoreModule
+  ) {
+    if(parentModule){
+      throw new Error('Core Modules has already been loaded. Import CoreModule into the AppModule only');
+    }
+  }
+
+}

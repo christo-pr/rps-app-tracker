ddaa1cdd44cec0516c4b3b87cfce5c833db897c7
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-04 09:20:54 -0600

    app/app.component.ts: Set the default lenguage and make use the device language

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Page } from 'ui/page';
+import { device } from 'platform';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'ns-app',
-    templateUrl: 'app.component.html',
+  selector: 'ns-app',
+  templateUrl: 'app.component.html',
 })
 
-export class AppComponent { }
+export class AppComponent {
+
+  myText: string;
+
+  constructor(private page: Page, private translateService: TranslateService) {
+
+    // Set the default translation to en-US
+    translateService.setDefaultLang('en-US');
+    // Set the device language.
+    translateService.use(device.language);
+  }
+
+}

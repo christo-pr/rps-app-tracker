e1aae8364bfe6d89dc410444a92a6cab1b9f8f23
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-04 09:19:59 -0600

    app/app.module.ts: imported the translate module and make it use our own factory

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { AppRoutingModule } from "./app.routing";
-import { AppComponent } from "./app.component";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Http } from '@angular/http';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NativeScriptHttpModule } from 'nativescript-angular/http';
+import { AppRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
 
-import { CoreModule } from "./core/core.module";
+import { CoreModule } from './core/core.module';
 
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+
+import { createTranslationLoader } from './utils';
 import './rxjs-imports';
 
 @NgModule({
@@ -14,7 +19,15 @@ import './rxjs-imports';
     imports: [
         NativeScriptModule,
         AppRoutingModule,
-        CoreModule
+        NativeScriptHttpModule,
+        CoreModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (createTranslationLoader),
+            deps: [Http]
+          }
+        })
     ],
     declarations: [
         AppComponent,

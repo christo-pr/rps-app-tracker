4e044c7c8110c49c93afb4a115f488c413b630c7
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-05 08:52:03 -0600

    app/globals/app-events.ts: Trigger the setStatusBarColor util function when the app launchs

--- app/globals/app-events.ts
+++ app/globals/app-events.ts
@@ -1,11 +1,12 @@
 import * as app from 'application';
 import { setNativeEvents } from './app-events-native';
+import { setStatusBarColor } from '../utils';
 
 export const setAppEvents = () => {
 
   setNativeEvents();
 
   app.on(app.launchEvent, (args: app.ApplicationEventData) => {
-
+    setStatusBarColor();
   });
 };

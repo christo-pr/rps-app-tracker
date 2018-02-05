14a5da04a7d37d98d98e1e3cf1e32458de0febff
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-05 09:06:55 -0600

    app/globals/app-events.ts: Modified the call of the setStatusBarColor function due the android bootstraping process, on resume will fit on both builds

--- app/globals/app-events.ts
+++ app/globals/app-events.ts
@@ -7,6 +7,18 @@ export const setAppEvents = () => {
   setNativeEvents();
 
   app.on(app.launchEvent, (args: app.ApplicationEventData) => {
+    /** Insert your launch events here. */
+  });
+
+
+  app.on(app.resumeEvent, (args: app.ApplicationEventData) => {
+    /** Insert your resume events here. */
+
+    /**
+     * We call it here because on android, the activity creatror
+     * happens be the resume event, but after the launch event, so on
+     * launch we wouldn't have the createActivity method.
+     */
     setStatusBarColor();
   });
 };

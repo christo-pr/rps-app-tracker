305d041a78b6d76da1201a50df58cf9f1fd313c6
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-10 08:09:36 -0600

    app/app.routing.ts: Fixed some errors

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,9 +1,9 @@
-import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" }
+    { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({

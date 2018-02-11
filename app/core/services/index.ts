a9667de97f5291746f59bde6a9afd3ab95b35151
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-11 14:10:23 -0600

    app/core/services/index.ts: Added the ServerErrorHandlerService to the barrel of the core/services

--- app/core/services/index.ts
+++ app/core/services/index.ts
@@ -1,10 +1,13 @@
-import { AuthService, LoggerService } from './';
+import { AuthService, LoggerService, ServerErrorHandlerService } from './';
 
 export * from './auth.service';
 export * from './logger.service';
 
+export * from './server-error-handler.service'
+
 export const SERVICES = [
   AuthService,
-  LoggerService
+  LoggerService,
+  ServerErrorHandlerService
 ];
 

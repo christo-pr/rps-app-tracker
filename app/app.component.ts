d04da5ea10ace320b004f0122c53dc2d206b805b
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-11 18:44:16 -0600

    app/app.component.ts: Removed some logic that doesnt belong to here

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,6 @@ import { TranslateService } from '@ngx-translate/core';
 import { LoggerService } from './core/services';
 import { Store } from './core/state/app-store';
 import { PtItem } from './core/models/domain';
-import { BacklogService } from './modules/backlog/services/backlog.service';
 
 @Component({
   selector: 'ns-app',
@@ -14,14 +13,11 @@ import { BacklogService } from './modules/backlog/services/backlog.service';
 
 export class AppComponent {
 
-  public items$ = this.store.select<PtItem[]>('backlogItems');
-
   constructor(
     private page: Page,
     private store: Store,
     private translateService: TranslateService,
-    private logger: LoggerService,
-    private backlogService: BacklogService
+    private logger: LoggerService
   ) {
     page.actionBarHidden = true;
     page.backgroundSpanUnderStatusBar = true;
@@ -31,7 +27,6 @@ export class AppComponent {
     // Set the device language.
     translateService.use(device.language);
 
-    this.backlogService.fetchItems();
   }
 
 }

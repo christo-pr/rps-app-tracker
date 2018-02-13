aafa4495a91a10cbfbd71969b75477e40f37d324
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-13 15:14:19 -0600

    app/core/services/index.ts: Created the barrel file to the core services

--- app/core/services/index.ts
+++ app/core/services/index.ts
@@ -1,13 +1,21 @@
-import { AuthService, LoggerService, ServerErrorHandlerService } from './';
+import { AuthService, LoggerService, ServerErrorHandlerService, AuthTokenService, StorageNsService, StorageWebService } from './';
 
 export * from './auth.service';
+export * from './auth-token.service';
 export * from './logger.service';
 
-export * from './server-error-handler.service'
+export * from './ns/storage-ns.service';
+export * from './web/storage-web.service';
+export * from './storage.service';
+
+export * from './server-error-handler.service';
 
 export const SERVICES = [
   AuthService,
+  AuthTokenService,
   LoggerService,
-  ServerErrorHandlerService
+  ServerErrorHandlerService,
+  StorageNsService,
+  StorageWebService
 ];
 

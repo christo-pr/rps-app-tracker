0ce4c61a3cba89a13b054fbb5790b9c8cadbc5b3
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-13 23:00:28 -0600

    app/app.routing.ts: Created some routes for testing purposes

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { Routes } from '@angular/router';
 
+import { BacklogPageComponent } from './modules/backlog/pages/backlog/backlog.page.component';
+import { LoginPageComponent } from './modules/auth/pages/login/login.page.component';
+import { DetailPageComponent } from './modules/backlog/pages/detail/detail.page.component';
+
 const routes: Routes = [
-    { path: '', redirectTo: '', pathMatch: 'full' }
+    { path: '', redirectTo: '/auth', pathMatch: 'full' },
+    { path: 'auth', component: LoginPageComponent },
+    { path: 'backlog', component: BacklogPageComponent },
+    { path: 'detail/:id', component: DetailPageComponent },
 ];
 
 @NgModule({

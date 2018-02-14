e398869f493f620b358077e508cdb8b07e95d2ee
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-13 22:19:47 -0600

    app/shared/components/menu/menu.component.ts: Created some methods for the menu component

--- app/shared/components/menu/menu.component.ts
+++ app/shared/components/menu/menu.component.ts
@@ -4,7 +4,8 @@ import { PresetType } from './../../models/ui/types/presets';
 @Component({
   moduleId: module.id,
   selector: 'pt-menu',
-  templateUrl: 'menu.component.html'
+  templateUrl: 'menu.component.html',
+  styleUrls: ['menu.component.css']
 })
 
 export class MenuComponent implements OnInit {
@@ -12,5 +13,11 @@ export class MenuComponent implements OnInit {
 
   ngOnInit() { }
 
-  onSelectedPresetTap(preset: PresetType) {}
+  public onSelectPresetTap(preset: PresetType) {
+    // this.navigationService.navigate(['backlog', preset]);
+  }
+
+  public onSettingsTap() {
+      // this.navigationService.navigate(['settings']);
+  }
 }

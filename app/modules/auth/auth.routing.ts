c794f02c9eaae3b8b608346e133f5ca5d581c852
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-14 11:09:31 -0600

    app/modules/auth/auth.routing.ts: Modified the routing file for the auth module to include two child routes prefix with auth

--- app/modules/auth/auth.routing.ts
+++ app/modules/auth/auth.routing.ts
@@ -3,10 +3,23 @@ import { Routes } from '@angular/router';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 
 import { LoginPageComponent, RegisterPageComponent } from './pages';
+import { AuthContainerComponent } from './containers';
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegisterPageComponent }
+  {
+    path: 'auth',
+    component: AuthContainerComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginPageComponent
+      },
+      {
+        path: 'register',
+        component: RegisterPageComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({

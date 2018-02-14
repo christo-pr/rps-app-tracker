158e5809809304bb6ae5482bf8c17eaef0cc868a
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-13 22:06:51 -0600

    app/modules/backlog/services/backlog.service.ts: Created some method to fetch a single item and to grab the avatar url

--- app/modules/backlog/services/backlog.service.ts
+++ app/modules/backlog/services/backlog.service.ts
@@ -1,14 +1,19 @@
-import { Injectable, NgZone } from '@angular/core';
+import { Injectable, NgZone, Inject } from '@angular/core';
+
+import { AppConfig } from '../../../core/models/core';
+import { APP_CONFIG } from '../../../config/app-config.module';
 
 import { BacklogRepository } from '../repositories/backlog.repository';
 import { Store } from '../../../core/state/app-store';
+
 import { ServerErrorHandlerService } from '../../../core/services';
-import { PtItem } from '../../../core/models/domain';
+import { PtItem, PtUser } from '../../../core/models/domain';
 
 @Injectable()
 export class BacklogService {
 
   constructor(
+    @Inject(APP_CONFIG) private config: AppConfig,
     private repo: BacklogRepository,
     private store: Store,
     private errorHandlerService: ServerErrorHandlerService,
@@ -19,8 +24,51 @@ export class BacklogService {
     this.repo.getPtItems(
       this.errorHandlerService.handleError,
       (ptItems: PtItem[]) => {
+        ptItems.forEach(i => {
+          this.setUserAvatarUrl(i.assignee);
+          i.comments.forEach(c => this.setUserAvatarUrl(c.user));
+        });
+
         this.zone.run(() => {
           this.store.set('backlogItems', ptItems);
+        });
+      }
+    );
+  }
+
+  public getItemFromCacheOrServer(id: number) {
+    // const selectedItem = _.find(this.store.value.backlogItems, i => i.id === id);
+    const selectedItem = this.store.value.backlogItems.find(i => i.id === id);
+    if (selectedItem) {
+      this.zone.run(() => {
+        this.store.set('currentSelectedItem', selectedItem);
+      });
+    } else {
+      this.getPtItem(id);
+    }
+  }
+
+  private setUserAvatarUrl(user: PtUser) {
+    user.avatar = `${this.config.apiEndpoint}/photo/${user.id}`;
+  }
+
+  public getPtItem(id: number) {
+    this.repo.getPtItem(id,
+      this.errorHandlerService.handleError,
+      (ptItem: PtItem) => {
+
+        this.setUserAvatarUrl(ptItem.assignee);
+        ptItem.comments.forEach(c => this.setUserAvatarUrl(c.user));
+
+        this.zone.run(() => {
+          this.store.set('currentSelectedItem', ptItem);
+
+          // optimistically update the item list with the new item
+          const updatedItems = this.store.value.backlogItems.map((item) => {
+            return item.id === id ? ptItem : item;
+          });
+
+          this.store.set('backlogItems', updatedItems);
         });
       }
     );

c7a4fadf4a3ef0f44bb6bca2cb3cf4f155fbf07d
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-13 22:07:36 -0600

    app/core/state/app-state.ts: Added some more properties to the state

--- app/core/state/app-state.ts
+++ app/core/state/app-state.ts
@@ -1,16 +1,19 @@
 import { PtUser } from './../models/domain';
 import { PtItem } from './../models/domain';
 
-export type StateKey = 'backlogItems' | 'currentUser';
+export type StateKey = 'backlogItems' | 'currentUser' | 
+'currentSelectedItem';
 
 
 export interface State {
   backlogItems: PtItem[];
   currentUser: PtUser;
+  currentSelectedItem: PtItem;
   [key: string]: any; // Positibilty to add key/value pairs
 }
 
 export const INITIAL_STATE: State = {
   backlogItems: [],
-  currentUser: undefined
+  currentUser: undefined,
+  currentSelectedItem: undefined
 };

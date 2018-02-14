a05391d82e84a8e0210754cece5044bc9fbdc027
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-14 09:55:21 -0600

    app/app.routing.ts: Clean up the app routing module

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,15 +2,8 @@ import { NgModule } from '@angular/core';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { Routes } from '@angular/router';
 
-import { BacklogPageComponent } from './modules/backlog/pages/backlog/backlog.page.component';
-import { LoginPageComponent } from './modules/auth/pages/login/login.page.component';
-import { DetailPageComponent } from './modules/backlog/pages/detail/detail.page.component';
-
 const routes: Routes = [
-    { path: '', redirectTo: '/auth', pathMatch: 'full' },
-    { path: 'auth', component: LoginPageComponent },
-    { path: 'backlog', component: BacklogPageComponent },
-    { path: 'detail/:id', component: DetailPageComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({

e46771e3b8da37709ce2d44245d5070ac89b08f5
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-15 12:56:19 -0600

    app/modules/backlog/components/index.ts: Modified the barrel file for the backlog components to also include the pt item details component

--- app/modules/backlog/components/index.ts
+++ app/modules/backlog/components/index.ts
@@ -1,8 +1,12 @@
 import { PtListComponent, PtListItemComponent } from './backlog';
+import { PtItemDetailsComponent } from './details/item-details/pt-item-details.component';
+
 
 export * from './backlog';
+export * from  './details/item-details/pt-item-details.component';
 
 export const COMPONENTS = [
   PtListComponent,
-  PtListItemComponent
+  PtListItemComponent,
+  PtItemDetailsComponent
 ];

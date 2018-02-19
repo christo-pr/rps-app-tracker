6833f0f160d17b2eec2369c54017be33061db977
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-18 23:40:52 -0600

    app/shared/helpers/ui-data-form/index.ts: Modified the barrel file with the new helper

--- app/shared/helpers/ui-data-form/index.ts
+++ app/shared/helpers/ui-data-form/index.ts
@@ -1,3 +1,4 @@
+export * from './picker-editor-helper/picker-editor-helper';
 export * from './segmented-editor-helper/segmented-editor-helper';
 export * from './stepper-editor-helper/stepper-editor-helper';
 export * from './multi-line-editor-helper/multi-line-editor-helper';

76e1a785a1b101f4b0348b61185e12e08f39c5c4
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-18 22:44:50 -0600

    app/shared/helpers/ui-data-form/*: Create the index file for the ui data form helpers fucntions and created the stepper implemenations for ios and android to change the native view

---
+++ app/shared/helpers/ui-data-form/stepper-editor-helper/stepper-editor-helper.android.ts
@@ -0,0 +1,66 @@
+export function setStepperEditorContentOffset(editor, horizontalOffset: number, verticalOffset: number) {
+
+}
+
+export function setStepperEditorTextPostfix(editor, singularPostfix: string, pluralPostifx: string) {
+  const numberPick:
+    com.telerik.widget.numberpicker.RadNumberPicker =
+    <com.telerik.widget.numberpicker.RadNumberPicker>editor.getEditorView();
+
+  const labelView = numberPick.labelView();
+  const numVal = parseInt(labelView.getText(), 10);
+  if (numVal === 1) {
+    labelView.setText(`1 ${singularPostfix}`);
+  } else {
+    labelView.setText(`${numVal} ${pluralPostifx}`);
+  }
+}
+
+
+export function setStepperEditorColors(editor, lightColor: Color, darkColor: Color): void {
+  const numberPicker: com.telerik.widget.numberpicker.RadNumberPicker =
+    <com.telerik.widget.numberpicker.RadNumberPicker>editor.getEditorView();
+
+  numberPicker.labelView().setTextColor(darkColor.android);
+  numberPicker.decreaseView().setTextColor(darkColor.android);
+  numberPicker.increaseView().setTextColor(darkColor.android);
+
+  const background1 = new android.graphics.drawable.GradientDrawable();
+  background1.setStroke(2, lightColor.android);
+  numberPicker.rootView().setBackground(background1);
+
+  const background2 = new android.graphics.drawable.GradientDrawable();
+  background2.setStroke(2, lightColor.android);
+  numberPicker.decreaseView().setBackground(background2);
+
+  const background3 = new android.graphics.drawable.GradientDrawable();
+  background3.setStroke(2, lightColor.android);
+  numberPicker.increaseView().setBackground(background3);
+}
+
+
+declare module com {
+  module telerik {
+    module android {
+      module common {
+        class Procedure<t> {
+          constructor(imp);
+        }
+      }
+    }
+    module widget {
+      class RadioGroup {
+        setPadding(l, t, r, b);
+      }
+      module numberpicker {
+        class RadNumberPicker {
+          rootView;
+          labelView();
+          decreaseView();
+          increaseView();
+        }
+      }
+    }
+  }
+
+}
---
+++ app/shared/helpers/ui-data-form/stepper-editor-helper/stepper-editor-helper.d.ts
@@ -0,0 +1,8 @@
+export function setStepperEditorContentOffset(editor, horizontalOffset: number,
+verticalOffset: number): void;
+
+export function setStepperEditorTextPostfix(editor, singularPostfix: string,
+pluralPostifx: string): void;
+
+export function setStepperEditorColors(editor, lightColor: Color,
+darkColor: Color): void;

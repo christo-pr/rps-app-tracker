1f7dc3f3336386ec6fe462fc7ce8258063083071
Cristofer Flores <cristofer.flort@gmail.com>
2018-02-18 21:30:50 -0600

    app/modules/backlog/components/details/item-details/pt-item-details.component.ts: Added the bussines logic in the component to trigger the update action in the parent page

--- app/modules/backlog/components/details/item-details/pt-item-details.component.ts
+++ app/modules/backlog/components/details/item-details/pt-item-details.component.ts
@@ -1,25 +1,75 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+
+import { DataFormEventData } from 'nativescript-pro-ui/dataform';
+import { RadDataFormComponent } from 'nativescript-pro-ui/dataform/angular';
 
 import { PtItem } from '../../../../../core/models/domain';
 import { PtItemDetailsEditFormModel, ptItemToFormModel } from '../../../../../shared/models/forms';
+import { ItemType, PT_ITEM_STATUSES, PT_ITEM_PRIORITIES } from '../../../../../core/constants';
+import { PtItemType } from '../../../../../core/models/domain/types';
 
 @Component({
-    moduleId: module.id,
-    selector: 'pt-item-details',
-    templateUrl: 'pt-item-details.component.html',
-    styleUrls: ['pt-item-details.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  moduleId: module.id,
+  selector: 'pt-item-details',
+  templateUrl: 'pt-item-details.component.html',
+  styleUrls: ['pt-item-details.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class PtItemDetailsComponent implements OnInit {
 
-    @Input() item: PtItem;
+  @Input() item: PtItem;
+  @Output() itemSaved = new EventEmitter<PtItem>();
+  @ViewChild('itemDetailsForm') itemDetailsForm: RadDataFormComponent;
 
-    public itemForm: PtItemDetailsEditFormModel;
+  private selectedTypeValue: PtItemType;
 
-    constructor() { }
+  public itemForm: PtItemDetailsEditFormModel;
 
-    ngOnInit() {
-        this.itemForm = ptItemToFormModel(this.item);
-    }
+  public itemTypesProvider = ItemType.List.map((t) => t.PtItemType);
+  public statusesProvider = PT_ITEM_STATUSES;
+  public prioritiesProvider = PT_ITEM_PRIORITIES;
+
+  public get itemTypeEditorDisplayName() {
+    return 'Type';
+  }
+
+  public itemTypeImage() {
+    return ItemType.imageResFromType(this.selectedTypeValue);
+  }
+
+
+  constructor() { }
+
+  public ngOnInit() {
+    this.itemForm = ptItemToFormModel(this.item);
+
+    this.selectedTypeValue = <PtItemType>this.itemForm.typeStr;
+  }
+
+  public onPropertyCommitted() {
+    this.notifyUpdatedItem();
+  }
+
+  private notifyUpdatedItem() {
+    this.itemDetailsForm.dataForm.validateAll()
+      .then(valid => {
+        if (valid) {
+          const updatedItem = this.getUpdatedItem();
+          this.itemSaved.emit(updatedItem);
+        }
+      })
+      .catch( err => console.error);
+  }
+
+  private getUpdatedItem(): PtItem {
+    return Object.assign({}, this.item, {
+      title: this.itemForm.title,
+      description: this.itemForm.description,
+      type: this.itemForm.typeStr,
+      status: this.itemForm.statusStr,
+      priority: this.itemForm.priorityStr,
+      estimate: this.itemForm.estimate
+    });
+  }
 }
